d4b97cc0aff82fdb9feb1ccada7941ff46ba24e8
taniadovzhenko <78045496+taniadovzhenko@users.noreply.github.com>
2021-02-03 09:05:35 +0200

    03.1.gg

---
+++ ConsoleApplication1.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+#include<math.h>
+double task_03_1gg(double x) {
+	double y = 16 * x * x * x * x + 8 * x * x * x + 4 * x * x + 2 * x + 1;
+	return y;
+}
+
+int main() {
+	double x, y;
+	scanf("%lf", &x);
+	y = task_03_1gg(x);
+	printf("y=%lf", y);
+}
