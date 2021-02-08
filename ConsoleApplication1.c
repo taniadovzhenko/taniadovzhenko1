fa8c2580c38226dc652a7920a0c1cb03e15333ab
taniadovzhenko <78045496+taniadovzhenko@users.noreply.github.com>
2021-02-08 21:00:49 +0200

    0.1.8b(homework 2)

---
+++ ConsoleApplication1.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include<limits.h>
+
+int main(void)
+{
+	{double m;
+	size_t m = sizeof(int);
+	long x;
+	size_t m = sizeof(x);
+	size_t m = sizeof('5')
+	}
+	double a;
+	a < 100;
+	scanf("%lf", &a);
+	printf("%lf ----- %lf", a, a, a);
+	printf("\n|%lf|", a);
+	printf("\n%lf ----- %lf", a, a);
+	return 0;
+}
