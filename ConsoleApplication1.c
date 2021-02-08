03ac409addcef8547fa7b6bc2ab41a833614a922
taniadovzhenko <78045496+taniadovzhenko@users.noreply.github.com>
2021-02-08 18:23:05 +0200

    task5b(homework 1)

---
+++ ConsoleApplication1.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	double a;
+	scanf("%lf", &a);
+	printf("%lf ----- %lf", a, a, a);
+	printf("\n|%lf|", a);
+	printf("\n%lf ----- %lf", a, a);
+	return 0;
+}
