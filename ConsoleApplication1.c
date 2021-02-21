958451746b6bb9c6ace24fa7a17c87b1675d044a
taniadovzhenko <78045496+taniadovzhenko@users.noreply.github.com>
2021-02-21 21:16:26 +0200

    дз.3 1.1.1

---
+++ ConsoleApplication1.c
@@ -0,0 +1,23 @@
+﻿/*Визначити більше та менше з двох чисел, введених з клавіатури */
+
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double x, y, min, max;
+	scanf("%lf", &x);
+	scanf("%lf", &y);
+	min = x;
+	max = x;
+	if (y < min) {
+		y = min;
+	}
+	if (y > max) {
+		y = max;
+	}
+	printf("%lf %lf", min, max);
+
+
+
+}
+
