a2bb16b6b4711a3169a1038a1122fe866fb904c7
taniadovzhenko <78045496+taniadovzhenko@users.noreply.github.com>
2021-02-21 21:15:30 +0200

    дз.3 0.3.6в

---
+++ ConsoleApplication1.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main() {
+	double x, f, h;
+	scanf("%lf", &x);
+	f = x / (1 + fabs(x));
+	h = ((-x * x / fabs(x)) + 1 + fabs(x)) / ((1 + fabs(x)) * (1 + fabs(x)));  //похідна
+	printf("%lf %lf", f, h);
+}
